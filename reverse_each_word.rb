d911a5323f0be67cc6a42fdd408ba7072980a18f
Will Carter <will.carter@fergusllc.com>
2019-12-20 18:40:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reversed_array = []
 
-  sentence_array.each do |word|
-    reversed_array << word.reverse!
-  end
+  # sentence_array.each do |word|
+  #   reversed_array << word.reverse!
+  # end
 
   #return reversed_array.join(" ")
 
