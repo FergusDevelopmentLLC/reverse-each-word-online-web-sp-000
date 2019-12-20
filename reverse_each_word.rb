81e4c2983abb48c2922996eee76464a7a4dbf8aa
Will Carter <will.carter@fergusllc.com>
2019-12-20 18:39:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,16 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reversed_array = []
+
   sentence_array.each do |word|
     reversed_array << word.reverse!
   end
-  reversed_array.join(" ")
+
+  return reversed_array.join(" ")
+
+  sentence_array.collect do |word|
+    word.reverse!
+  end
 end
 
 puts reverse_each_word("Hello there, and how are you?")
